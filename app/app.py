47290b0b04082eb155a59369cfb216ca7c2434c0
krn-zorel <kumar.karanq98@gmail.com>
2026-04-27 00:37:06 +0530

    shifting to modular structure

---
+++ app/app.py
@@ -0,0 +1,5 @@
+# Run:  python app.py   (from the project root)
+# Deps: pip install dash dash-bootstrap-components plotly pandas
+#       numpy scikit-learn joblib
+import dash
+import dash_bootstrap_components as dbc
